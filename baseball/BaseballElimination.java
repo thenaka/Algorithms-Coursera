06847041b859c219d75912e8d930ef742227ae67
Ryan Muranaka <rjmuranaka@gmail.com>
2023-09-08 15:52:24 -0600

    Completed assignment

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
-
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination {
     private int teamCount;
     private String[] teams;
-    private String[] vertices;
+    private String[] teamsAndGames;
     private int[] wins;
     private int[] losses;
     private int[] remaining;
@@ -60,7 +60,7 @@ public class BaseballElimination {
      *                                     be found in {@code teams}.
      */
     public int wins(String team) {
-        ValidateTeamIsNotNull(team);
+        validateTeamIsNotNull(team);
         return wins[getTeamNumber(team)];
     }
 
@@ -73,7 +73,7 @@ public class BaseballElimination {
      *                                     be found in {@code teams}.
      */
     public int losses(String team) {
-        ValidateTeamIsNotNull(team);
+        validateTeamIsNotNull(team);
         return losses[getTeamNumber(team)];
     }
 
@@ -86,7 +86,7 @@ public class BaseballElimination {
      *                                     be found in {@code teams}.
      */
     public int remaining(String team) {
-        ValidateTeamIsNotNull(team);
+        validateTeamIsNotNull(team);
         return remaining[getTeamNumber(team)];
     }
 
@@ -101,8 +101,8 @@ public class BaseballElimination {
      *                                     be found in {@code teams}.
      */
     public int against(String team1, String team2) {
-        ValidateTeamIsNotNull(team1);
-        ValidateTeamIsNotNull(team2);
+        validateTeamIsNotNull(team1);
+        validateTeamIsNotNull(team2);
         return gamesVersus[getTeamNumber(team1)][getTeamNumber(team2)];
     }
 
@@ -115,13 +115,36 @@ public class BaseballElimination {
      *                                     be found in {@code teams}.
      */
     public boolean isEliminated(String team) {
-        ValidateTeamIsNotNull(team);
+        validateTeamIsNotNull(team);
+
+        eliminators = new Bag<String>();
+
         if (isTriviallyEliminated(team)) {
             return true;
         }
-        setupFlowNetwork(team);
 
-        return false;
+        FordFulkerson fordFulkerson = setupFlowNetwork(team);
+        for (int i = 0; i < (teams.length - 1); i++) {
+            if (fordFulkerson.inCut(i)) {
+                eliminators.add(teamsAndGames[i]);
+            }
+        }
+
+        return !eliminators.isEmpty();
+    }
+
+    private boolean isTriviallyEliminated(String team) {
+        int teamNumber = getTeamNumber(team);
+        int totalPossible = wins(team) + remaining(team);
+        for (int i = 0; i < teamCount; i++) {
+            if (teamNumber == i) {
+                continue;
+            }
+            if (totalPossible < wins[i]) {
+                eliminators.add(teams[i]);
+            }
+        }
+        return !eliminators.isEmpty();
     }
 
     /**
@@ -134,7 +157,7 @@ public class BaseballElimination {
      *                                     be found in {@code teams}.
      */
     public Iterable<String> certificateOfElimination(String team) {
-        ValidateTeamIsNotNull(team);
+        validateTeamIsNotNull(team);
         isEliminated(team);
         return eliminators.isEmpty() ? null : eliminators;
     }
@@ -164,23 +187,40 @@ public class BaseballElimination {
         }
     }
 
-    private void setupFlowNetwork(String team) {
-        // Flow network will have one vertex for each team and a vertex for the games
-        // between each team.
-        // For the following example:
+    private FordFulkerson setupFlowNetwork(String team) {
+        teamsAndGames = new String[getTeamsAndGamesCount()];
+        fillTeamsAndGames(team);
+
+        FlowNetwork flowNetwork = new FlowNetwork(teamsAndGames.length + 2);
+
+        connectFlowNetwork(team, flowNetwork);
+
+        return new FordFulkerson(flowNetwork, teamsAndGames.length, teamsAndGames.length + 1);
+    }
+
+    private int getTeamsAndGamesCount() {
+        // Number of vertices in the flow network example:
+        // New_York 75 59 28 0 3 8 7 3
+        // Baltimore 71 63 28 3 0 2 7 7
+        // Boston 69 66 27 8 2 0 0 3
+        // Toronto 63 72 27 7 7 0 0 3
+        // Detroit 49 86 27 3 7 3 3 0
         //
-        // Team Wins Losses Remaining Versus
-        // 0 Atlanta        83 71 8     0 1 6 1
-        // 1 Philadelphia   80 79 3     1 0 0 2
-        // 2 New_York       78 78 6     6 0 0 0
-        // 3 Montreal       77 82 3     1 2 0 0
+        // Number of teams minus one: 4
+        // Plus: +
+        // Games against each other: 4 * 4 = 16
+        // Minus the games against themselves: 16 - 4 = 12
+        // Divide the remainder by two: 12 / 2 = 6
+        // Equals: = 10 vertices
 
-        int vertexCount = teamCount - 1;
-        for (int i = teamCount - 2; i > 0; i--) {
-            vertexCount += i;
-        }
-        vertices = new String[vertexCount];
+        int remainingTeams = teamCount - 1;
+        int gamesVersusCount = ((remainingTeams * remainingTeams) - remainingTeams) / 2;
+        int teamsAndGamesCount = remainingTeams + gamesVersusCount;
 
+        return teamsAndGamesCount;
+    }
+
+    private void fillTeamsAndGames(String team) {
         int teamNumber = getTeamNumber(team);
         int vertexIndex = 0;
         int teamIndex = 0;
@@ -191,7 +231,7 @@ public class BaseballElimination {
                 teamIndex++;
                 continue;
             }
-            vertices[vertexIndex++] = teams[teamIndex++];
+            teamsAndGames[vertexIndex++] = teams[teamIndex++];
         }
 
         // Fill the remaining vertices with the teams' games versus each other
@@ -200,20 +240,42 @@ public class BaseballElimination {
                 if (i == j) {
                     continue; // skip team's games against themself
                 }
-                vertices[vertexIndex++] = vertices[i] + "-" + vertices[j];
+                teamsAndGames[vertexIndex++] = teamsAndGames[i] + "-" + teamsAndGames[j];
             }
         }
+    }
 
-        FlowNetwork flowNetwork = new FlowNetwork(vertices.length + 2);
+    private void connectFlowNetwork(String team, FlowNetwork flowNetwork) {
+        // Connect teams to the target with capacity of (wins + remaining - candidate
+        // team wins)
+        int candidateTeam = getTeamNumber(team);
+        int remainingTeamCount = teamCount - 1;
+        int targetVertex = teamsAndGames.length + 1;
+        for (int i = 0; i < remainingTeamCount; i++) {
+            flowNetwork.addEdge(new FlowEdge(i, targetVertex,
+                    wins[candidateTeam] + remaining[candidateTeam] - wins[getTeamNumber(teamsAndGames[i])]));
+        }
 
-        // source vertex = vertices.length
-        // target vertex = vertices.length + 1
+        // Connect flow of games versus to each team playing with infinite capacity
+        for (int i = remainingTeamCount; i < teamsAndGames.length; i++) {
+            String[] matchup = teamsAndGames[i].split("-");
+            int team1 = getTeamsAndGamesNumber(matchup[0]);
+            int team2 = getTeamsAndGamesNumber(matchup[1]);
 
+            flowNetwork.addEdge(new FlowEdge(i, team1, Double.POSITIVE_INFINITY));
+            flowNetwork.addEdge(new FlowEdge(i, team2, Double.POSITIVE_INFINITY));
+        }
 
+        // Connect flow from source to games versus with the capacity equal to the
+        // number of games against
+        int sourceVertex = teamsAndGames.length;
+        for (int i = remainingTeamCount; i < teamsAndGames.length; i++) {
+            String[] matchup = teamsAndGames[i].split("-");
+            int team1 = getTeamNumber(matchup[0]);
+            int team2 = getTeamNumber(matchup[1]);
 
-
-        // FordFulkerson maxFlow = new FordFulkerson(null, teamCount, teamCount)
-
+            flowNetwork.addEdge(new FlowEdge(sourceVertex, i, gamesVersus[team1][team2]));
+        }
     }
 
     private int getTeamNumber(String team) {
@@ -225,28 +287,33 @@ public class BaseballElimination {
         throw new IllegalArgumentException(team + " not found");
     }
 
-    private void ValidateTeamIsNotNull(String team) {
+    private int getTeamsAndGamesNumber(String team) {
+        for (int i = 0; i < (teamCount - 1); i++) {
+            if (teamsAndGames[i].equals(team)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException(team + " not found");
+    }
+
+    private void validateTeamIsNotNull(String team) {
         if (team == null) {
             throw new IllegalArgumentException("Team name cannot be null");
         }
     }
 
-    private boolean isTriviallyEliminated(String team) {
-        int teamNumber = getTeamNumber(team);
-        int totalPossible = wins(team) + remaining(team);
-        for (int i = 0; i < teamCount; i++) {
-            if (teamNumber == i) {
-                continue;
-            }
-            if (totalPossible < wins[i]) {
-                eliminators.add(teams[i]);
+    public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            } else {
+                StdOut.println(team + " is not eliminated");
             }
         }
-        return eliminators.size() > 0;
-    }
-
-    public static void main(String[] args) {
-        BaseballElimination baseballElimination = new BaseballElimination("teams/teams4.txt");
-        baseballElimination.isEliminated("Atlanta");
     }
 }
